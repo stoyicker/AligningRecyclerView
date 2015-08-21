7afbc4781e19852fb6e8d062d9996a7be5d8df9e
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2015-08-21 20:06:22 +0200

    Added a ProgressBar to the layout. Some fixes.

--- sample/src/main/java/io/github/stoyicker/annoyodroid/sample/ui/activity/MainActivity.java
+++ sample/src/main/java/io/github/stoyicker/annoyodroid/sample/ui/activity/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.ScrollingMovementMethod;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -41,6 +42,9 @@ public class MainActivity extends AppCompatActivity {
     @Bind(R.id.button_async)
     Button mButtonASync;
 
+    @Bind(R.id.progress_bar)
+    View mProgressBar;
+
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,11 +56,14 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.button_async)
     void requestAsync() {
-        final int position = mMethodSpinner.getSelectedItemPosition();
+        setResponseVisibility(false);
+        //You'll have to instatiate it in the switch statement
         final Callback<Object> genericCallback = new Callback<Object>() {
+
             @Override
-            public void success(final @Nullable Object o, final Response response) {
-                MainActivity.this.setText(response.getStatus() + ":" + (o != null ? o.toString() : "Empty response received"));
+            public void success(final Object o, final Response response) {
+                MainActivity.this.setText((o != null ? o.toString() : "Empty response received"));
+                MainActivity.this.setResponseVisibility(true);
             }
 
             @Override
@@ -65,43 +72,82 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.setText(response.getStatus() + ":" + response.getReason());
             }
         };
+        final int position;
+
+        switch (position = mMethodSpinner.getSelectedItemPosition()) {
+            default:
+                throw new IllegalArgumentException("Position " + position + " not properly aligned with a method");
+        }
+    }
+
+    private void setResponseVisibility(final boolean b) {
+        mProgressBar.setVisibility(b ? View.INVISIBLE : View.VISIBLE);
+        mResponseField.setVisibility(b ? View.VISIBLE : View.INVISIBLE);
     }
 
     @OnClick(R.id.button_sync)
     void requestSync() {
-        final int position = mMethodSpinner.getSelectedItemPosition();
+        setResponseVisibility(false);
         new AsyncTask<Void, Void, Object>() {
+
+            private int mPosition;
+
+            private AsyncTask<Void, Void, Object> init(int position) {
+                mPosition = position;
+                return this;
+            }
+
             @Override
             protected Object doInBackground(final Void... params) {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException ignored) {
+                }
+                switch (mPosition) {
+                    default:
+//                throw new IllegalArgumentException("Position " + position + " not properly aligned with a method");
+                }
                 return null;
             }
 
             @Override
             protected void onPostExecute(final @Nullable Object o) {
                 MainActivity.this.setText((o != null ? o.toString() : "Empty response received"));
+                MainActivity.this.setResponseVisibility(true);
             }
-        }.executeOnExecutor(Executors.newSingleThreadExecutor());
+        }.init(mMethodSpinner.getSelectedItemPosition()).executeOnExecutor(Executors.newSingleThreadExecutor());
     }
 
     @OnClick(R.id.button_rxandroid)
     void requestRx() {
+        setResponseVisibility(false);
+        //You'll have to instatiate in the switch statement
+        Observable<Object> genericCallback = null;
+        final int position;
+
+        switch (position = mMethodSpinner.getSelectedItemPosition()) {
+            default:
+//                throw new IllegalArgumentException("Position " + position + " not properly aligned with a method");
+        }
         Observable.just(null)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Observer<Object>() {
                     @Override
                     public void onCompleted() {
-
+                        MainActivity.this.setResponseVisibility(true);
                     }
 
                     @Override
                     public void onError(final Throwable e) {
                         MainActivity.this.setText(e.getMessage());
+                        MainActivity.this.setResponseVisibility(true);
                     }
 
                     @Override
                     public void onNext(final Object o) {
                         MainActivity.this.setText((o != null ? o.toString() : "Empty response received"));
+
                     }
                 });
     }
